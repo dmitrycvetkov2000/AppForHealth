07045a4506277410a70d07f537816507b94bf502
dmitrycvetkov2000 <dimacvetkov2000@mail.ru>
2023-05-07 08:34:00 +0300

    add Colors

---
+++ AppForHealth/Colors/UIColorExtention.swift
@@ -0,0 +1,54 @@
+//
+//  UIColorExtention.swift
+//  AppForHealth
+//
+//  Created by Дмитрий Цветков on 07.04.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var tabBarItemAccent: UIColor {
+        #colorLiteral(red: 0.1752927601, green: 0.02862494253, blue: 0.1602545977, alpha: 1)
+    }
+    static var tabBarGreenColor: UIColor {
+        #colorLiteral(red: 0, green: 0.3492194414, blue: 0, alpha: 1)
+    }
+    static var tabBarItemNotAccent: UIColor {
+        #colorLiteral(red: 0.1753531098, green: 0.02764961869, blue: 0.1609031856, alpha: 0.6003541252)
+    }
+    static var titleColor: UIColor {
+        #colorLiteral(red: 0, green: 0.3492194414, blue: 0, alpha: 1)
+    }
+    static var buttonBackColor: UIColor {
+        #colorLiteral(red: 0, green: 0.3492194414, blue: 0, alpha: 1)
+    }
+    static var forButtonBorder: UIColor {
+        #colorLiteral(red: 0, green: 0.2676816881, blue: 0, alpha: 1)
+    }
+    static var forMiniButtonBorder: UIColor {
+        #colorLiteral(red: 0.2340404689, green: 0.04845609516, blue: 0.1046315357, alpha: 1)
+    }
+    static var forMiniButtonBorderNoActive: UIColor {
+        #colorLiteral(red: 0.2340404689, green: 0.04845609516, blue: 0.1046315357, alpha: 0.5)
+    }
+    static var forJustText: UIColor {
+        #colorLiteral(red: 0.1752927601, green: 0.02862494253, blue: 0.1602545977, alpha: 1)
+    }
+    static var activeColor: UIColor {
+        #colorLiteral(red: 0, green: 0.3492194414, blue: 0, alpha: 1)
+    }
+    static var noActiveColor: UIColor {
+        #colorLiteral(red: 0.2095280886, green: 0.03551472723, blue: 0.08309682459, alpha: 0.1669036663)
+    }
+    static var tabBarMainColor: UIColor {
+        #colorLiteral(red: 0.3220285177, green: 0.2310446501, blue: 0.6010909677, alpha: 1)
+    }
+    static var tabBarTintColor: UIColor {
+        #colorLiteral(red: 0.01797287539, green: 0.2065472007, blue: 0.01784341037, alpha: 1)
+    }
+    static var tabBarHalfTintColor: UIColor {
+        #colorLiteral(red: 0.01797287539, green: 0.2065472007, blue: 0.01784341037, alpha: 0.5)
+    }
+    
+}
