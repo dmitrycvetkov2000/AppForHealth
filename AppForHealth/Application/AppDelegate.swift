2654033e987e5fe6eda680e7d97542798bedef49
dmitrycvetkov2000 <dimacvetkov2000@mail.ru>
2023-05-07 08:33:24 +0300

    change Application/AppDelegate

--- AppForHealth/Application/AppDelegate.swift
+++ AppForHealth/Application/AppDelegate.swift
@@ -8,72 +8,89 @@
 import UIKit
 import CoreData
 import Firebase
+import RealmSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
     
     private let splashPresenter = SplashScreenPresenter()
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        
-        splashPresenter.present()
+        self.splashPresenter.present()
         let window = UIWindow()
         
         let vc = AuthorizationModuleBuilder.build()
-        
+
         window.rootViewController = vc
         window.makeKeyAndVisible()
         
         DispatchQueue.global().async { // Делаем какую-либо работу
-            FirebaseApp.configure()
-
             for i in 0...1000000 {
-                print(i*i)
+                if i == 0 {
+                    DispatchQueue.global().async {
+                        DispatchQueue.main.async {
+                            FirebaseApp.configure()
+                            self.createFodsForBD()
+                        }
+                    }
+                }
+                //print(i*i)
                 if i % 10000 == 0 {
-                    self.splashPresenter.notify(percent: Float(i) / 1000000.0)
+                    //DispatchQueue.main.async {
+                        self.splashPresenter.notify(percent: Float(i) / 1000000.0)
+                    //}
                 }
             }
-            
+
             DispatchQueue.main.async {
-                if Core.shared.isNewUser() {
+                                if CoreDataManager.instance.isEmptyCoreData() && (Auth.auth().currentUser != nil) {
+                                    let vc = ParametrsModuleBuilder.build()
+                                    window.rootViewController = vc
+                                    window.makeKeyAndVisible()
+                                } else if !CoreDataManager.instance.isEmptyCoreData() && (Auth.auth().currentUser != nil) {
+                                    let vc = MainTabBarModuleBuilder.build(usingNavigationFactory: NavigationBuilder.build)
+                                    window.rootViewController = vc
+                                    window.makeKeyAndVisible()
+                                } else if Core.shared.isNewUser() && CoreDataManager.instance.isEmptyCoreData() && (Auth.auth().currentUser == nil) {
+                                    let vc = OnboardModuleBuilder.build()
+                                    window.rootViewController = vc
+                                    window.makeKeyAndVisible()
+                                } else if CoreDataManager.instance.isEmptyCoreData() && (Auth.auth().currentUser == nil) {
+                                    let vc = AuthorizationModuleBuilder.build()
+                                    window.rootViewController = vc
+                                    window.makeKeyAndVisible()
+                                }
+                self.window = window
                     DispatchQueue.main.async {
-                            let vc = OnboardModuleBuilder.build()
-
-                            window.rootViewController = vc
-                            window.makeKeyAndVisible()
+                        self.splashPresenter.dismiss()
                     }
-                } else {
-                    DispatchQueue.main.async {
-                        
-                        if CoreDataManager.instance.isEmptyCoreData() && (Auth.auth().currentUser != nil) {
-                            let vc = ParametrsModuleBuilder.build()
-                            window.rootViewController = vc
-                            window.makeKeyAndVisible()
-                        } else if !CoreDataManager.instance.isEmptyCoreData() && (Auth.auth().currentUser != nil) {
-                            let vc = MainModuleBuilder.build()
-                            window.rootViewController = vc
-                            window.makeKeyAndVisible()
-                        }
-                        
-                        Auth.auth().addStateDidChangeListener { auth, user in
-                            if user == nil { // Если пользователь вышел или не зарегистрирован
-                                let vc = AuthorizationModuleBuilder.build()
-                                window.rootViewController = vc
-                                window.makeKeyAndVisible()
-
-                            }
-                        }
-                    }
-                }
-                self.splashPresenter.dismiss()
             }
         }
-        self.window = window
         return true
     }
+    
+    func createFodsForBD() {
+        let realm = try! Realm()
+        if realm.isEmpty {
+            let value1 = Product(value: ["Банан".localized(), 42.5, 12.5, 23.4, 10])
+            let value2 = Product(value: ["Апельсин".localized(), 4.5, 2.5, 3.4, 20])
+            let value3 = Product(value: ["Яблоко".localized(), 2.5, 1.5, 2.4, 30])
+            let value4 = Product(value: ["Киви".localized(), 10.5, 6.9, 2.1, 40])
+            let value5 = Product(value: ["Мороженное".localized(), 23, 14.5, 53.4, 50])
+            let value6 = Product(value: ["Ананас".localized(), 0.5, 6.5, 8.4, 60])
+            let value7 = Product(value: ["Арбуз".localized(), 40.5, 32.5, 3.4, 70])
+            let value8 = Product(value: ["Дыня".localized(), 5.5, 5.5, 5.4, 80])
+            
+            let values = [value1, value2, value3, value4, value5, value6, value7, value8]
+            
+            try! realm.write {
+                for val in values {
+                    realm.add(val)
+                }
+            }
+        }
+    }
+    
 //    func applicationDidFinishLaunching(_ application: UIApplication) {
 //        let window = UIWindow()
 //        let vc = AuthorizationModuleBuilder.build()
